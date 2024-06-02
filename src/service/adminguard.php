94c093841427b84940c8eb2255df521c1a36ee34
cppmonger <keskinemirhan@gmail.com>
2024-06-03 02:23:22 +0300

    Add database control to guards

--- src/service/adminguard.php
+++ src/service/adminguard.php
@@ -1,8 +1,13 @@
 <?php
+include "service/dbconnect.php";
 $adminLogin = "admin-login.php";
 
 session_start();
-if (!isset($_SESSION["adminId"]) || !isset($_SESSION["isLoggedIn"])) {
+if (
+    !isset($_SESSION["adminId"]) ||
+    !isset($_SESSION["isLoggedIn"]) ||
+    $stmt_execute("select * from admin where id = ?", "s", $_SESSION["adminId"])->num_rows < 1
+) {
     header("Location: " . $adminLogin);
     exit();
 }
--- src/service/userguard.php
+++ src/service/userguard.php
@@ -1,8 +1,13 @@
 <?php
+include "service/dbconnect.php";
 $adminLogin = "login.php";
 
 session_start();
-if (!isset($_SESSION["userId"]) || !isset($_SESSION["isLoggedIn"])) {
+if (
+    !isset($_SESSION["userId"]) ||
+    !isset($_SESSION["userIsLoggedIn"]) ||
+    $stmt_execute("select * from user where id = ?", "s", $_SESSION["userId"])->num_rows < 1
+) {
     header("Location: " . $userLogin);
     exit();
 }
