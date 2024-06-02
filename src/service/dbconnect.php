4e04acffeb479fe814ab7c964d4795c4d4d7de4e
cppmonger <keskinemirhan@gmail.com>
2024-06-03 02:27:26 +0300

    Add dbconnect include control

--- src/service/dbconnect.php
+++ src/service/dbconnect.php
@@ -1,15 +1,18 @@
 <?php
-$hostname = "127.0.0.1";
-$username = "root";
-$password = "";
-$database = "eticdot";
-$port = 3306;
-$mysqli = new mysqli($hostname, $username, null, $database, $port);
-$stmt_execute = function (string $sql, string $types, string ...$args) use ($mysqli) {
-    $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param($types, ...$args);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $stmt->close();
-    return $result;
-};
+
+if (!isset($mysqli)) {
+    $hostname = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $database = "eticdot";
+    $port = 3306;
+    $mysqli = new mysqli($hostname, $username, null, $database, $port);
+    $stmt_execute = function (string $sql, string $types, string ...$args) use ($mysqli) {
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param($types, ...$args);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        return $result;
+    };
+}
